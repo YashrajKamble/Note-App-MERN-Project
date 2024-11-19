505ef34087c6a3d2d2315130690f123b15a1edc2
Yashraj_K <yashrajkamble7727@gamil.com>
2024-11-19 15:35:57 +0530

    up to create user schema

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,18 @@
+require("dotenv").config();
+
+const config = require("./config.json");
+const mongoose = require("mongoose");
+
+mongoose.connect(config.connectionString)
+    .then(() => {
+        console.log("MongoDB connected successfully");
+    })
+    .catch((err) => {
+        console.error("MongoDB connection error:", err);
+    });
+
 const express = require("express");
-
 const cors = require("cors");
-
 const app = express();
 
 app.use(express.json());
@@ -20,4 +31,4 @@ app.listen(8000, () => {
     console.log("Server is running on port 8000");
 });
 
-module.exports = app; 
+module.exports = app;
